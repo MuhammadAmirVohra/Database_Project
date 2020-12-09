da4b5328f1a80e37654c4414ab237e55d4de500d
MuhammadAmirVohra <k180124@nu.edu.pk>
2020-12-09 22:11:25 +0500

    Heroku Port

--- app.js
+++ app.js
@@ -159,9 +159,12 @@ app.post('/:id/delete', checkAuthenticated, (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server Started at PORT : 5000");
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
 });
+
 
 
 
